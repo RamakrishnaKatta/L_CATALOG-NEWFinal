c05d9e8ba1cc47b3947e71274e6710857bede8b1
ramky <ramky.lll@outlook.com>
2018-02-21 15:59:23 +0530

    The url has been updated from  "http://isl-lb-1935712655.ap-south-1.elb.amazonaws.com"  to ladmin.immersionslabs.com

--- lcatalog/src/main/java/com/immersionslabs/lcatalog/Utils/EnvConstants.java
+++ lcatalog/src/main/java/com/immersionslabs/lcatalog/Utils/EnvConstants.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class EnvConstants {
 
-    public static final String APP_BASE_URL = "http://isl-lb-1935712655.ap-south-1.elb.amazonaws.com";
+    public static final String APP_BASE_URL = "http://ladmin.immersionslabs.com/";
 
     public static ArrayList<String> user_Favourite_list = new ArrayList<String>();
     public static String user_type = null;
