501bd20efa17fcb3fbd2f666466dee92bf80e086
Naganath <nagnath.m@immersionslabs.com>
2018-03-09 10:37:44 +0530

    product feedback ratings and feedback post api calls updated

--- lcatalog/src/main/java/com/immersionslabs/lcatalog/Fragment_ProductFeedback.java
+++ lcatalog/src/main/java/com/immersionslabs/lcatalog/Fragment_ProductFeedback.java
@@ -13,13 +13,24 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.immersionslabs.lcatalog.Utils.EnvConstants;
+import com.immersionslabs.lcatalog.Utils.SessionManager;
 import com.immersionslabs.lcatalog.network.ApiCommunication;
+import com.immersionslabs.lcatalog.network.ApiService;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class Fragment_ProductFeedback extends Fragment implements View.OnClickListener, ApiCommunication {
 
@@ -27,11 +38,19 @@ public class Fragment_ProductFeedback extends Fragment implements View.OnClickLi
 
     EditText feedback_article_id, feedback_vendor_id, feedback_message, feedback_article_name;
     TextInputLayout feedback_article_id_area, feedback_vendor_id_area, feedback_message_area, feedback_article_name_area;
-    Button feedback_submit,ratings_submit;
+    Button feedback_submit, ratings_submit;
     RatingBar ratingBar;
-    String f_article_id, f_vendor_id, f_message, f_article_name;
-    RelativeLayout feedback_layout,ratings_layout;
-    ImageView feedback_imageview,ratings_imageview;
+    String f_article_id, f_vendor_id, f_message, f_article_name, user_id;
+    RelativeLayout feedback_layout, ratings_layout;
+    ImageView feedback_imageview, ratings_imageview;
+    SessionManager sessionManager;
+    String resp, code, message;
+    Float rating = 2.5f;
+
+    public static final String RATING_URL = EnvConstants.APP_BASE_URL + "articleRating";
+    public static final String ARTICLE_FEEDBACK_URL = "http://ladmin.immersionslabs.com/articleFeedback";
+    public static final String GET_RATING_URL = EnvConstants.APP_BASE_URL + "getUserRating";
+    private String global_user_id;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -50,10 +69,12 @@ public class Fragment_ProductFeedback extends Fragment implements View.OnClickLi
         feedback_imageview = view.findViewById(R.id.feedback_imageview);
         feedback_submit = view.findViewById(R.id.feedback_submit);
 
-        ratingBar=view.findViewById(R.id.ratingsbar);
-        ratings_layout=view.findViewById(R.id.ratings_relative_layout);
-        ratings_submit=view.findViewById(R.id.ratings_submit);
-        ratings_imageview=view.findViewById(R.id.ratings_imageview);
+        ratingBar = view.findViewById(R.id.ratingsbar);
+        ratings_layout = view.findViewById(R.id.ratings_relative_layout);
+        ratings_submit = view.findViewById(R.id.ratings_submit);
+        ratings_imageview = view.findViewById(R.id.ratings_imageview);
+
+        sessionManager = new SessionManager(getContext());
 
         feedback_article_id_area.setVisibility(View.GONE);
         feedback_vendor_id_area.setVisibility(View.GONE);
@@ -72,7 +93,6 @@ public class Fragment_ProductFeedback extends Fragment implements View.OnClickLi
         feedback_submit.setOnClickListener(this);
 
 
-
         f_article_id = getArguments().getString("article_id");
         Log.e(TAG, "--" + f_article_id);
         feedback_article_id.setText(f_article_id);
@@ -84,6 +104,12 @@ public class Fragment_ProductFeedback extends Fragment implements View.OnClickLi
         f_article_name = getArguments().getString("article_title");
         Log.e(TAG, "--" + f_article_name);
         feedback_article_name.setText(f_article_name);
+
+        HashMap userdetails = new HashMap();
+        userdetails = sessionManager.getUserDetails();
+        user_id = String.valueOf(userdetails.get(SessionManager.KEY_USER_ID));
+        global_user_id = String.valueOf(userdetails.get(SessionManager.KEY_GLOBAL_USER_ID));
+
 
         feedback_layout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,15 +137,13 @@ public class Fragment_ProductFeedback extends Fragment implements View.OnClickLi
         ratings_layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ratings_layout.getTag().toString().equals("down"))
-                {
+                if (ratings_layout.getTag().toString().equals("down")) {
                     ratingBar.setVisibility(View.VISIBLE);
                     ratings_submit.setVisibility(View.VISIBLE);
                     ratings_imageview.setImageResource(R.mipmap.ic_faq_up);
                     ratings_layout.setTag("up");
 
-                }
-                else {
+                } else {
                     ratings_imageview.setImageResource(R.mipmap.ic_faq_down);
                     ratingBar.setVisibility(View.GONE);
                     ratings_submit.setVisibility(View.GONE);
@@ -127,41 +151,87 @@ public class Fragment_ProductFeedback extends Fragment implements View.OnClickLi
                 }
             }
         });
+        ratings_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    ratingapicall();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
 
         return view;
+    }
+
+    public void onResume() {
+        super.onResume();
+    }
+
+
+    private void ratingapicall() throws JSONException {
+        float rating = ratingBar.getRating();
+        JSONObject rating_parameters = new JSONObject();
+        rating_parameters.put("article_id", f_article_id);
+        rating_parameters.put("user_id", user_id);
+        rating_parameters.put("rating", rating);
+        ApiService.getInstance(getContext()).postData(this, RATING_URL, rating_parameters, "RATING", "ARTICLE_RATING");
+
+    }
+
+    private void feedbackapicall() throws JSONException {
+        String message = feedback_message.getText().toString();
+        JSONObject feedback_parameters = new JSONObject();
+        feedback_parameters.put("article_id", f_article_id);
+        feedback_parameters.put("user_id", global_user_id);
+        feedback_parameters.put("vendor_id", f_vendor_id);
+        feedback_parameters.put("feedbacks", message);
+        Log.e(TAG, "feedbackapicall: Request" + feedback_parameters);
+
+        ApiService.getInstance(getContext()).postData(this, ARTICLE_FEEDBACK_URL, feedback_parameters, "FEEDBACK_ARTICLE", "ARTICLE_FEEDBACK");
     }
 
     @Override
     public void onClick(View v) {
 
-        if(v.getId()==R.id.feedback_submit)
-        {
-            Toast.makeText(getActivity(), "Your submit form is accepted", Toast.LENGTH_LONG).show();
+        if (v.getId() == R.id.feedback_submit) {
+            try {
+                feedbackapicall();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+//            Toast.makeText(getActivity(), "Your submit form is accepted", Toast.LENGTH_LONG).show();
         }
-        else if(v.getId()==R.id.ratings_submit)
-        {
-            Toast.makeText(getActivity(), "Your submit form is accepted", Toast.LENGTH_LONG).show();
-        }
-
-   //     switch (v.getId()) {
-//            case R.id.feedback_submit:
-//
-//                Toast.makeText(getActivity(), "Your submit form is accepted", Toast.LENGTH_LONG).show();
-//
-//
-//            case R.id.ratings_submit:
-//
-//                Toast.makeText(getActivity(), "Your Rating is submitted.Thank you. ", Toast.LENGTH_LONG).show();
-//
-//        }
-
     }
-
 
     @Override
     public void onResponseCallback(JSONObject response, String flag) {
+        if (flag.equals("ARTICLE_RATING")) {
+            try {
 
+                message = response.getString("message");
+                Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+                Log.e(TAG, "Response--" + resp + " Status Code--" + code + " Message--" + message);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else if (flag.equals("ARTICLE_FEEDBACK")) {
+            try {
+
+                message = response.getString("message");
+                Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
+                Log.e(TAG, "Response--" + resp + " Status Code--" + code + " Message--" + message);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 
     @Override
     public void onErrorCallback(VolleyError error, String flag) {
