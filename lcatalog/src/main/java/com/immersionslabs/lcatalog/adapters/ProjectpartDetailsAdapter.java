4960ff0b36aa709e3741f5a10caa821ca59918e7
ramky <ramky.lll@outlook.com>
2018-04-10 17:00:19 +0530

    Project part Details Images Updated

--- lcatalog/src/main/java/com/immersionslabs/lcatalog/adapters/ProjectpartDetailsAdapter.java
+++ lcatalog/src/main/java/com/immersionslabs/lcatalog/adapters/ProjectpartDetailsAdapter.java
@@ -2,6 +2,8 @@ package com.immersionslabs.lcatalog.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.immersionslabs.lcatalog.ProductPageActivity;
 import com.immersionslabs.lcatalog.ProjectpartDetailsActivity;
 import com.immersionslabs.lcatalog.R;
 import com.immersionslabs.lcatalog.Utils.EnvConstants;
@@ -51,26 +54,33 @@ public class ProjectpartDetailsAdapter extends RecyclerView.Adapter<ProjectpartD
     }
 
     @Override
-    public void onBindViewHolder(ProjectpartDetailsAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(ProjectpartDetailsAdapter.ViewHolder holder, final int position) {
         final Context[] context = new Context[1];
         String im1 = null;
         String get_image = part_article_images.get(position);
-        Log.e(TAG, "project_images" + get_image);
+        Log.e(TAG, "project_images"  + get_image);
 
         holder.article_name.setText(part_article_name.get(position));
-        try {
-            JSONObject images_json = new JSONObject(get_image);
-            im1 = images_json.getString("img");
-            Log.e(TAG, "onBindViewHolder: image" + im1);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
         Glide.with(activity)
-                .load(EnvConstants.APP_BASE_URL + "/upload/images/" + im1)
+                .load(EnvConstants.APP_BASE_URL + "/upload/images/" + get_image)
                 .placeholder(R.drawable.dummy_icon)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(holder.article_image);
+
+        holder.part_article_container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context[0] = v.getContext();
+
+                Intent intent = new Intent(context[0], ProductPageActivity.class);
+                Bundle b = new Bundle();
+
+                b.putString("article_id", part_articles_id.get(position));
+                intent.putExtras(b);
+                context[0].startActivity(intent);
+
+            }
+        });
     }
 
     @Override
@@ -81,13 +91,14 @@ public class ProjectpartDetailsAdapter extends RecyclerView.Adapter<ProjectpartD
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView article_name;
         private ImageView article_image;
-        private RelativeLayout part_articel_container;
+        private RelativeLayout part_article_container;
 
         public ViewHolder(View itemView) {
 
             super(itemView);
             article_name = itemView.findViewById(R.id.part_article_name);
             article_image = itemView.findViewById(R.id.part_article_image);
+            part_article_container = itemView.findViewById(R.id.part_article_container);
 
 
         }
